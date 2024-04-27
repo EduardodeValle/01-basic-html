1b40823946c6180c18ebf5eae4101f35b6599f3d
Eduardo de Valle <EduardodeValle@users.noreply.github.com>
2024-04-27 17:27:59 -0600

    Favorite page implemented with local storage

---
+++ 04-nestjs-rick-and-morty/src/app/favorites/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import CharacterCard from "../components/CharacterCard";
+import { Character } from "../types";
+import NavBar from "../components/navbar";
+
+export default function Home() {
+  const [favoriteCharacters, setFavoriteCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    const fetchFavoriteCharacters = async () => {
+      const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');
+
+      // Gather all character IDs from favorites
+      const characterIds = Object.keys(favorites).map(key => parseInt(key, 10));
+
+      // Fetch all characters based on these IDs
+      const fetchedCharacters = await Promise.all(
+        characterIds.map(async (id) => {
+          const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+          const data = await response.json();
+          return {
+            ...data,
+            isFavorite: true // Ensure the fetched characters are marked as favorites
+          };
+        })
+      );
+
+      setFavoriteCharacters(fetchedCharacters);
+    };
+
+    fetchFavoriteCharacters();
+  }, []);
+
+  return (
+    <main className="overflow-hidden">
+      <NavBar></NavBar>
+      <div className="mt-12 px-4 py-2 max-w-screen-xl mx-auto" id="discover">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {favoriteCharacters.map((character, index) => (
+            <CharacterCard key={index} character={character} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
