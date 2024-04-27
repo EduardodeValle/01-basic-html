0a800c93361ea91cef832ce73b3c1e62d6fe7995
Eduardo de Valle <EduardodeValle@users.noreply.github.com>
2024-04-27 12:25:09 -0600

    Cards improved

--- 04-nestjs-rick-and-morty/src/components/CharacterCard.tsx
+++ 04-nestjs-rick-and-morty/src/components/CharacterCard.tsx
@@ -11,13 +11,13 @@ const CharacterCard = ({ character }: CharacterCardProps) => {
   console.log(character);
   const { name, status, species, location, image } = character;
   return (
-    <div className="bg-white rounded-lg">
-      <img src={image} alt="character image" width={200} height={200} className="rounded-lg" />
-      <div className="p-6">
-        <h4>{name}</h4>
+    <div className="bg-white rounded-lg overflow-hidden">
+      <img src={image} alt="character image" width={300} height={300} className="rounded-xl object-cover" />
+      <div className="max-w-sm p-2">
+        <h4 className="text-lg font-semibold truncate">{name}</h4> {/* Ensures long names are truncated gracefully */}
         <div>
-          <p>{status} - {species}</p>
-          <p>Location: {location.name}</p>
+          <p className="text-sm text-gray-600 truncate">{status} - {species}</p> {/* Truncate ensures text fits on one line */}
+          <p className="text-sm text-gray-600 break-words">Location: {location.name}</p> {/* Truncate ensures text fits on one line */}
         </div>
       </div>
     </div>
